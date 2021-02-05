25bc6f391f6678fe92d9ff9239a02ed1c7e75929
kotenei <kotenei@qq.com>
2021-02-05 17:50:30 +0800

    update

--- app/index.js
+++ app/index.js
@@ -1,17 +1,11 @@
 import { React, ReactDOM } from "mini-react";
 
-const updateValue = (e) => {
-  rerender(e.target.value);
-};
+function Counter() {
+  // const [state, setState] = useState(1);
+  // return <h1 onClick={() => setState((c) => c + 1)}>Count: {state}</h1>;
+  return <div>Hi</div>
+}
 
-const rerender = (value) => {
-  const element = (
-    <div>
-      <input onInput={updateValue} value={value} />
-      <h2>Hello {value}</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById("root"));
-};
-
-rerender("World")
+const element = <Counter />;
+const container = document.getElementById("root");
+ReactDOM.render(element, container);
--- src/react-dom/render.js
+++ src/react-dom/render.js
@@ -2,6 +2,8 @@ let nextUnitOfWork = null;
 let currentRoot = null;
 let wipRoot = null;
 let deletions = null;
+let wipFiber = null;
+let hookIndex = null;
 
 /**
  * 给 fiber 节点创建其 dom 节点和设置其属性
@@ -38,25 +40,37 @@ const commitWork = (fiber) => {
     return;
   }
 
-  const domParent = fiber.parent.dom;
+  let domParentFiber = fiber.parent;
+  while (!domParentFiber.dom) {
+    domParentFiber = domParentFiber.parent;
+  }
+
+  const domParent = domParentFiber.dom;
 
   if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
     domParent.appendChild(fiber.dom);
   } else if (fiber.effectTag === "UPDATE" && fiber.dom !== null) {
     updateDom(fiber.dom, fiber.alternate.props, fiber.props);
   } else if (fiber.effectTag === "DELETION") {
-    domParent.removeChild(fiber.dom);
+    commitDeletion(fiber, domParent);
   }
 
   commitWork(fiber.child);
   commitWork(fiber.sibling);
 };
 
+const commitDeletion = (fiber, domParent) => {
+  if (fiber.dom) {
+    domParent.removeChild(fiber.dom);
+  } else {
+    commitDeletion(fiber.child, domParent);
+  }
+};
+
 const isEvent = (key) => key.startsWith("on");
 const isProperty = (key) => key !== "children" && !isEvent(key);
 const isNew = (prev, next) => (key) => prev[key] !== next[key];
 const isGone = (prev, next) => (key) => !(key in next);
-
 const updateDom = (dom, prevProps, nextProps) => {
   // 移除旧的或已修改过的事件
   Object.keys(prevProps)
@@ -132,12 +146,12 @@ const workloop = (deadline) => {
  * @param {*} fiber
  */
 const performUnitOfWork = (fiber) => {
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber);
+  const isFunctionComponent = fiber.type instanceof Function;
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber);
+  } else {
+    updateHostComponent(fiber);
   }
-
-  const elements = fiber.props.children;
-  reconcileChildren(fiber, elements);
 
   // 如果当前 fiber 节点有子节点则返回子节点
   if (fiber.child) {
@@ -152,6 +166,53 @@ const performUnitOfWork = (fiber) => {
     }
     nextFiber = nextFiber.parent;
   }
+};
+
+// const useState = (initial) => {
+//   const oldHook =
+//     wipFiber.alternate &&
+//     wipFiber.alternate.hooks &&
+//     wipFiber.alternate.hooks[hookIndex];
+
+//   const hook = {
+//     state: oldHook ? oldHook.state : initial,
+//     queue: [],
+//   };
+
+//   const actions = oldHook ? oldHook.queue : [];
+//   actions.forEach((action) => {
+//     hook.state = action(hook.state);
+//   });
+
+//   const setState = (action) => {
+//     hook.queue.push(action);
+//     wipRoot = {
+//       dom: currentRoot.dom,
+//       props: currentRoot.props,
+//       alternate: currentRoot,
+//     };
+//     nextunitOfWork = wipRoot;
+//     deletions = [];
+//   };
+
+//   wipFiber.hooks.push(hook);
+//   hookIndex++;
+//   return [hook.state, setState];
+// }
+
+const updateFunctionComponent = (fiber) => {
+  wipFiber = fiber;
+  hookIndex = 0;
+  wipFiber.hooks = [];
+  const children = [fiber.type(fiber.props)];
+  reconcileChildren(fiber, children);
+};
+
+const updateHostComponent = (fiber) => {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  reconcileChildren(fiber, fiber.props.children);
 };
 
 /**
